8ada9054d9ef872e16d7ce67cdd82e1f70ec2292
Youssef <rafrafi.youssef@hotmail.com>
2022-02-19 10:30:17 +0100

    first push

---
+++ opengl-project/libs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<iostream>
+#include"glew.h"
+#include"glfw3.h"
+#include"glm.hpp"
+#include"vec2.hpp"
+#include"vec3.hpp"
+#include"vec4.hpp"
+#include"mat4x4.hpp"
+#include"gtc/matrix_transform.hpp"
+#include"gtc/type_ptr.hpp"
+#include"SOIL2.h"
